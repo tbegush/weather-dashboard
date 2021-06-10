9d56c97662b3de70e918f822af3d0f5f6365a991
Thomas Begush <tbegush@gmail.com>
2021-06-10 15:48:34 -0700

    added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+$("#futureWeather").hide();
+$("#currentWeather").hide();
+$("#searchHistory").hide();
+var lat;
+var lon;
+
 var searchTerm = document.querySelector("#searchTerm").value;
 
 var weatherData;
@@ -25,50 +31,85 @@ function getWeather() {
     .then(function (response) {
       weatherData = response;
       console.log(weatherData);
+      $("#currentWeather").show();
+      $("#searchHistory").show();
+lat = response.coord.lat;
+lon = response.coord.lon;
+console.log(lat);
 
       localStorage.setItem(searchTerm, JSON.stringify(response));
-
+      $("#weatherIcon").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
       $("#cityName").find("span").html(searchTerm);
       $("#temperature").find("span").html(response.main.temp);
 
-      $("#searchHistory").append("<button class='cityButton' data-city='"+searchTerm+"'>"+searchTerm+"</button>");
+      $("#humidity").find("span").html(response.main.humidity);
+      $("#windSpeed").find("span").html(response.wind.speed);
 
+      $("#searchHistory").append("<button class='cityButton' data-city='+searchTerm+'>"+searchTerm+"</button>");
+      $("#todaysDate").html(todaysDate);
+
+      getForecast(searchTerm);
     });
-    
 };
 
 
-$(".cityButton").on("click",
-function () {
-    var searchTerm = $(this).data("city");
-    console.log(searchTerm);
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        searchTerm +
-        "&appid=5b0024e8f1352a96b8859e9d81eabcea&units=imperial"
-    )
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (response) {
-        weatherData = response;
-        console.log(weatherData);
-  
-        localStorage.setItem(searchTerm, JSON.stringify(response));
-  
-        $("#cityName").find("span").html(searchTerm);
-        $("#temperature").find("span").html(response.main.temp);
-  
 
-  
-      });
+
+
+function getForecast(searchTerm) {
+  // var searchTerm = document.querySelector("#searchTerm").value;
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      searchTerm +
+      "&appid=5b0024e8f1352a96b8859e9d81eabcea&units=imperial"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      forecastData = response;
+      console.log(forecastData);
+
+      localStorage.setItem(searchTerm, JSON.stringify(response));
+$("#futureWeather").show();
+
+      $("#forecastCityName").find("span").html(searchTerm);
+      $("#forecastTemperature").find("span").html(response.list[4].main.temp);
+      $("#forecastHumidity").find("span").html(response.list[4].main.humidity);
+      $("#forecastWindSpeed").find("span").html(response.list[4].wind.speed);
+      $("#forecastDate").find("span").html(response.list[4].dt_txt);
+
+      $("#forecastCityName2").find("span").html(searchTerm);
+      $("#forecastTemperature2").find("span").html(response.list[12].main.temp);
+      $("#forecastHumidity2").find("span").html(response.list[12].main.humidity);
+      $("#forecastWindSpeed2").find("span").html(response.list[12].wind.speed);
+      $("#forecastDate2").find("span").html(response.list[12].dt_txt);
+
+      $("#forecastCityName3").find("span").html(searchTerm);
+      $("#forecastTemperature3").find("span").html(response.list[20].main.temp);
+      $("#forecastHumidity3").find("span").html(response.list[20].main.humidity);
+      $("#forecastWindSpeed3").find("span").html(response.list[20].wind.speed);
+      $("#forecastDate3").find("span").html(response.list[20].dt_txt);
+
+      $("#forecastCityName4").find("span").html(searchTerm);
+      $("#forecastTemperature4").find("span").html(response.list[28].main.temp);
+      $("#forecastHumidity4").find("span").html(response.list[28].main.humidity);
+      $("#forecastWindSpeed4").find("span").html(response.list[28].wind.speed);
+      $("#forecastDate4").find("span").html(response.list[28].dt_txt);
+
+      $("#forecastCityName5").find("span").html(searchTerm);
+      $("#forecastTemperature5").find("span").html(response.list[36].main.temp);
+      $("#forecastHumidity5").find("span").html(response.list[36].main.humidity);
+      $("#forecastWindSpeed5").find("span").html(response.list[36].wind.speed);
+      $("#forecastDate5").find("span").html(response.list[36].dt_txt);
       
-  }
+    });
 
- );
+};
 
+$(".cityButton").on("click", getWeather);
 
-
+ // hit enter - if the search term is "changed" this will work.  
 $("#searchTerm").on("change", getWeather);
 
 $(document).ready(function () {
