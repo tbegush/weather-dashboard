7b19978b8e01d6755ea9ec4dc0ce3fb6846c32fe
Thomas Begush <tbegush@gmail.com>
2021-06-10 18:21:51 -0700

    added uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,8 @@ $("#todaysDate").text(todaysDate);
 //this grabs the weather from the API and pushes to the HTML
 function getWeather() {
   var searchTerm = document.querySelector("#searchTerm").value;
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
       searchTerm +
       "&appid=5b0024e8f1352a96b8859e9d81eabcea&units=imperial"
   )
@@ -26,29 +27,56 @@ function getWeather() {
     })
     .then(function (response) {
       weatherData = response;
-      console.log(weatherData);
       //show the search history and current weather at this point
       $("#currentWeather").show();
       $("#searchHistory").show();
-lat = response.coord.lat;
-lon = response.coord.lon;
+      lat = response.coord.lat;
+      lon = response.coord.lon;
 
       localStorage.setItem(searchTerm, JSON.stringify(response));
-      $("#weatherIcon").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+      $("#weatherIcon")
+        .find("img")
+        .attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            response.weather[0].icon +
+            ".png"
+        );
       $("#cityName").find("span").html(searchTerm);
       $("#date").find("span").html(todaysDate);
       $("#temperature").find("span").html(response.main.temp);
       $("#humidity").find("span").html(response.main.humidity);
       $("#windSpeed").find("span").html(response.wind.speed);
-      $("#searchHistory").append("<button class='cityButton' data-city='+searchTerm+'>"+searchTerm+"</button>");
+      $("#searchHistory").append(
+        "<button class='cityButton' data-city='+searchTerm+'>" +
+          searchTerm +
+          "</button>"
+      );
       $("#todaysDate").html(todaysDate);
 
       getForecast(searchTerm);
+      getUvIndex();
     });
-};
+}
+
+function getUvIndex() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=current,minutely,hourly,alerts&appid=5b0024e8f1352a96b8859e9d81eabcea"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (abc) {
+      $("#uvIndex").find("span").html(abc.daily[0].uvi);
+      console.log(abc);
+    });
+}
 
 function getForecast(searchTerm) {
-  // var searchTerm = document.querySelector("#searchTerm").value;
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
       searchTerm +
@@ -59,7 +87,6 @@ function getForecast(searchTerm) {
     })
     .then(function (response) {
       forecastData = response;
-      console.log(forecastData);
 
       localStorage.setItem(searchTerm, JSON.stringify(response));
       $("#futureWeather").show();
@@ -69,39 +96,80 @@ function getForecast(searchTerm) {
       $("#forecastHumidity").find("span").html(response.list[4].main.humidity);
       $("#forecastWindSpeed").find("span").html(response.list[4].wind.speed);
       $("#forecastDate").find("span").html(response.list[4].dt_txt);
-      $("#forecastweatherIcon").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.list[3].weather.icon + ".png");
+      $("#forecastweatherIcon")
+        .find("img")
+        .attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            response.list[3].weather.icon +
+            ".png"
+        );
 
       $("#forecastTemperature2").find("span").html(response.list[12].main.temp);
-      $("#forecastHumidity2").find("span").html(response.list[12].main.humidity);
+      $("#forecastHumidity2")
+        .find("span")
+        .html(response.list[12].main.humidity);
       $("#forecastWindSpeed2").find("span").html(response.list[12].wind.speed);
       $("#forecastDate2").find("span").html(response.list[12].dt_txt);
-      $("#forecastWeatherIcon2").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.list[11].weather.icon + ".png");
+      $("#forecastWeatherIcon2")
+        .find("img")
+        .attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            response.list[11].weather.icon +
+            ".png"
+        );
 
       $("#forecastTemperature3").find("span").html(response.list[20].main.temp);
-      $("#forecastHumidity3").find("span").html(response.list[20].main.humidity);
+      $("#forecastHumidity3")
+        .find("span")
+        .html(response.list[20].main.humidity);
       $("#forecastWindSpeed3").find("span").html(response.list[20].wind.speed);
       $("#forecastDate3").find("span").html(response.list[20].dt_txt);
-      $("#forecastWeatherIcon3").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.list[20].weather.icon  + ".png");
-      
+      $("#forecastWeatherIcon3")
+        .find("img")
+        .attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            response.list[20].weather.icon +
+            ".png"
+        );
+
       $("#forecastTemperature4").find("span").html(response.list[28].main.temp);
-      $("#forecastHumidity4").find("span").html(response.list[28].main.humidity);
+      $("#forecastHumidity4")
+        .find("span")
+        .html(response.list[28].main.humidity);
       $("#forecastWindSpeed4").find("span").html(response.list[28].wind.speed);
       $("#forecastDate4").find("span").html(response.list[28].dt_txt);
-      $("#forecastWeatherIcon4").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.list[28].weather.icon + ".png");
+      $("#forecastWeatherIcon4")
+        .find("img")
+        .attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            response.list[28].weather.icon +
+            ".png"
+        );
 
       $("#forecastTemperature5").find("span").html(response.list[36].main.temp);
-      $("#forecastHumidity5").find("span").html(response.list[36].main.humidity);
+      $("#forecastHumidity5")
+        .find("span")
+        .html(response.list[36].main.humidity);
       $("#forecastWindSpeed5").find("span").html(response.list[36].wind.speed);
       $("#forecastDate5").find("span").html(response.list[36].dt_txt);
-      $("#forecastWeatherIcon5").find("img").attr("src", "http://openweathermap.org/img/wn/" + response.list[36].weather.icon  + ".png");
-            
+      $("#forecastWeatherIcon5")
+        .find("img")
+        .attr(
+          "src",
+          "http://openweathermap.org/img/wn/" +
+            response.list[36].weather.icon +
+            ".png"
+        );
     });
-
-};
+}
 
 $(".cityButton").on("click", getWeather);
 
- // hit enter - if the search term is "changed" this will work.  
+// hit enter - if the search term is "changed" this will work.
 $("#searchTerm").on("change", getWeather);
 
 $(document).ready(function () {
